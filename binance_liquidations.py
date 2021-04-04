27afd389740ada1eb9e9640999883fbc8c04951c
David Arroyo <darroyolpz@users.noreply.github.com>
2021-04-04 12:19:08 +0100

    Threads functionality for funding function
    Funding value exported to a txt file, then retrieved when liquidation message

--- binance_liquidations.py
+++ binance_liquidations.py
@@ -1,14 +1,14 @@
 # https://binance-docs.github.io/apidocs/futures/en/#all-market-liquidation-order-streams
-import time, dateparser, json, sys, os, requests, websocket
+import time, dateparser, json, sys, os, requests, websocket, threading
 from discord_webhook import DiscordWebhook
 from datetime import datetime
 import pandas as pd
 
 # Web-socket
 base_url = "wss://fstream.binance.com"
-coin = "BTC" # Change if needed
-stream =  coin.lower() + "usdt@forceOrder"
-socket = base_url + "/ws/" + stream
+symbol = "BTCUSDT" # Change if needed
+stream =  "@forceOrder"
+socket = f"{base_url}/ws/{symbol.lower()}{stream}"
 
 # Webhook settings
 url_wb = os.environ.get('DISCORD_WH')
@@ -18,15 +18,31 @@ def ms_to_date(trade_time):
 	trade_time = datetime.fromtimestamp(trade_time/1000.0)
 	return trade_time
 
-# Funding function
-def funding_function(coin):
-	url = 'https://fapi.binance.com/fapi/v1/premiumIndex?symbol=' + coin.upper() + 'USDT'
-	response = requests.get(url).text
-	value = json.loads(response)
-	funding = 100*float(value['lastFundingRate'])
+# Funding function to export as txt
+def funding_function(symbol="BTCUSDT"):
+	# Run this function forever
+	while True:
+		url = f"https://fapi.binance.com/fapi/v1/premiumIndex?symbol={symbol.upper()}" 
+		response = requests.get(url).text
+		value = json.loads(response)
+		funding = str(100*float(value['lastFundingRate']))
+
+		# Write funding for txt
+		with open('funding.txt', 'w+') as w:
+			w.write(funding)
+
+		# Wait during ten minutes
+		timestamp_print = datetime.now().strftime("%d-%m-%Y %H:%M:%S") # For console
+		print(f"{timestamp_print} || New funding = {funding}%")
+		time.sleep(600)
+
+# Get funding from txt
+def get_funding():
+	with open('funding.txt', 'r') as f:
+		funding = float(f.read())
 	return funding
 
-# Main function
+# What to do when a message arrives function
 def read_msg(ws, msg):
 	# Get data from web-socket
 	timestamp = datetime.now() # For delay
@@ -39,13 +55,19 @@ def read_msg(ws, msg):
 	trade_time = ms_to_date(int(json.loads(msg)['o']['T']))
 	delay = timestamp - trade_time
 	usd = amount*price/1000 # In thousands
+
+	# Get funding
+	funding = get_funding()
 	emoji = ":robot:"
 	alert_msg = ""
 
-	# For last minute PA
+	# For high funding or last minute PA
 	if minute >= 55:
 		emoji = ":warning:"
 		alert_msg = " - Last min PA"
+	elif funding > 0.075:
+		emoji = ":warning:"
+		alert_msg = " - High funding"
 
 	# Check if long or short
 	if side == "SELL":
@@ -59,17 +81,23 @@ def read_msg(ws, msg):
 	if usd > 900: # In thousands
 		emoji = ":lion:"
 		alert_msg = alert_msg + " - REKT BIGLY"
-		ending = ending + ":skull_crossbones:"
+		ending = ":skull_crossbones:" + ending
 
 	# Print timestamp and message
-	msg_discord = f"{emoji} **{direction}{alert_msg}** | ${usd:.1f}k at {price:.0f} {ending}"
+	msg_discord = f"{emoji} **{direction}{alert_msg}** | ${usd:.1f}k at {price:.0f} | {funding:.3f}% {ending}"
 	print(f"{timestamp_print} | Delay: {delay} | {msg_discord}")
 
 	# Discord message for big liquidations
 	usd_limit = 100 # In thousands
-	if usd > usd_limit:		
+	if usd > usd_limit:
 		webhook = DiscordWebhook(url=url_wb, content=msg_discord)
 		response = webhook.execute()
 
-ws = websocket.WebSocketApp(socket, on_message=read_msg)
-ws.run_forever()
+# Main websocket_function
+def websocket_function():
+	ws = websocket.WebSocketApp(socket, on_message=read_msg)
+	ws.run_forever()
+
+# Block for threads
+threading.Thread(target=funding_function).start()
+threading.Thread(target=websocket_function).start()
